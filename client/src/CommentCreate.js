16e09b83de7731450d9ef72b2857e24a243f4d01
Daylan <daylanberry@gmail.com>
2020-08-01 07:48:58 -0700

    creating query service

--- client/src/CommentCreate.js
+++ client/src/CommentCreate.js
@@ -1,15 +1,30 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const CommentCreate = ({ postId }) => {
 
+  const [content, setContent] = useState('')
+
+
+  const onSubmit = async (e) => {
+    e.preventDefault(e)
+
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content
+    });
+
+    setContent('');
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className='form-group'>
           <label>New Comment</label>
           <input
             className='form-control'
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
           />
           <button className='btn btn-primary'>Submit</button>
         </div>
